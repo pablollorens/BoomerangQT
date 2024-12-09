0ac658880e1ddcd49affffe17dc2e04b26990cae
_eFe_ <nmefedb@gmail.com>
2024-12-09 17:11:27 +0100

    Added Telegram notification

--- BoomerangQT.Orders.cs
+++ BoomerangQT.Orders.cs
@@ -665,6 +665,9 @@ namespace BoomerangQT
                 // Llamada al método para enviar el email desde el otro archivo parcial
                 SendTradeClosedEmail(position.Symbol.Name, closedPnL, lastMaxDrawdown);
 
+                // Llamada al método para enviar mensaje a telegram desde el otro archivo parcial
+                System.Threading.Tasks.Task tgTask = SendTradeClosedTelegramMsg(position.Symbol.Name, closedPnL, lastMaxDrawdown);
+
                 //Log($"OnPositionRemove event called - Position {position.Id} has been closed.", StrategyLoggingLevel.Trading);
 
                 // Cancel any remaining orders associated with the position
